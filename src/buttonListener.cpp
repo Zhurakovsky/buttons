b6f028fce5546861d8decadc4c63a00d4444784e
Yurii Zhurakovskyi <yzhurakovskyi@luxoft.com>
2017-08-28 22:38:27 +0800

    Set pulldown for input pins 5

--- src/buttonListener.cpp
+++ src/buttonListener.cpp
@@ -161,6 +161,8 @@ void ButtonListener::processButton(const uint32_t &valueMask)
                         }
                         else
                         {
+                            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                            iter->second = 0;
                             //Protection from button slippage
                         }
                     }

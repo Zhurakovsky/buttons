e6805c14c96048270fd4a29c9339680542810bf8
Yurii Zhurakovskyi <yzhurakovskyi@luxoft.com>
2017-09-01 12:52:37 +0800

    Try to revert 3

--- src/menubuilder.cpp
+++ src/menubuilder.cpp
@@ -169,85 +169,85 @@ void MenuBuilder::buildButtonsFuncAssigned(const std::string &fileName, std::map
     }
 }
 
-void MenuBuilder::setParentItem(std::vector<MenuItem*> &menu, MenuItem *processedMenuItem, uint32_t parentId)
-{
-    // By default menu item already have nullptr as parent
-    if (parentId == 0)
-    {
-        return;
-    }
+//void MenuBuilder::setParentItem(std::vector<MenuItem*> &menu, MenuItem *processedMenuItem, uint32_t parentId)
+//{
+//    // By default menu item already have nullptr as parent
+//    if (parentId == 0)
+//    {
+//        return;
+//    }
 
-    for (auto it = menu.begin(); it != menu.end(); ++it)
-    {
-        MenuItem *testedItem = *it;
-        uint32_t testedParent = testedItem->getItemId();
-        if (parentId == testedParent)
-        {
-            if (processedMenuItem->getParent() == nullptr)
-            {
-                processedMenuItem->setParent(testedItem);
-            }
-            if (testedItem->getChild() == nullptr)
-            {
-                testedItem->setChild(processedMenuItem);
-            }
-        }
-    }
-}
+//    for (auto it = menu.begin(); it != menu.end(); ++it)
+//    {
+//        MenuItem *testedItem = *it;
+//        uint32_t testedParent = testedItem->getItemId();
+//        if (parentId == testedParent)
+//        {
+//            if (processedMenuItem->getParent() == nullptr)
+//            {
+//                processedMenuItem->setParent(testedItem);
+//            }
+//            if (testedItem->getChild() == nullptr)
+//            {
+//                testedItem->setChild(processedMenuItem);
+//            }
+//        }
+//    }
+//}
 
-void MenuBuilder::setLeftItem(std::vector<MenuItem*> &menu, MenuItem *processedMenuItem, uint32_t leftId)
-{
+//void MenuBuilder::setLeftItem(std::vector<MenuItem*> &menu, MenuItem *processedMenuItem, uint32_t leftId)
+//{
 
-    // By default menu item already have nullptr as previous
-    if (leftId == 0)
-    {
-        return;
-    }
+//    // By default menu item already have nullptr as previous
+//    if (leftId == 0)
+//    {
+//        return;
+//    }
 
-    for (auto it = menu.begin(); it != menu.end(); ++it)
-    {
-        MenuItem *testedItem = *it;
-        uint32_t testedLeft = testedItem->getItemId();
-        if (leftId == testedLeft)
-        {
-            //In our context we consider Left as Previous
-            if (processedMenuItem->getPrevious() == nullptr)
-            {
-                processedMenuItem->setPrevious(testedItem);
-            }
-            if (testedItem->getNext() == nullptr)
-            {
-                testedItem->setNext(processedMenuItem);
-            }
-        }
-    }
-}
+//    for (auto it = menu.begin(); it != menu.end(); ++it)
+//    {
+//        MenuItem *testedItem = *it;
+//        uint32_t testedLeft = testedItem->getItemId();
+//        if (leftId == testedLeft)
+//        {
+//            //In our context we consider Left as Previous
+//            if (processedMenuItem->getPrevious() == nullptr)
+//            {
+//                processedMenuItem->setPrevious(testedItem);
+//            }
+//            if (testedItem->getNext() == nullptr)
+//            {
+//                testedItem->setNext(processedMenuItem);
+//            }
+//        }
+//    }
+//}
 
-void MenuBuilder::setRightItem(std::vector<MenuItem*> &menu, MenuItem *processedMenuItem, uint32_t rightId)
-{
+//void MenuBuilder::setRightItem(std::vector<MenuItem*> &menu, MenuItem *processedMenuItem, uint32_t rightId)
+//{
 
-    // By default menu item already have nullptr as next
-    if (rightId == 0)
-    {
-        return;
-    }
+//    // By default menu item already have nullptr as next
+//    if (rightId == 0)
+//    {
+//        return;
+//    }
 
-    for (auto it = menu.begin(); it != menu.end(); ++it)
-    {
-        MenuItem *testedItem = *it;
-        uint32_t testedRight = testedItem->getItemId();
-        if (rightId == testedRight)
-        {
-            //In our context we consider Left as Previous
-            if (processedMenuItem->getNext() == nullptr)
-            {
-                processedMenuItem->setNext(testedItem);
-            }
-            if (testedItem->getPrevious() == nullptr)
-            {
-                testedItem->setPrevious(processedMenuItem);
-            }
-        }
-    }
-}
+//    for (auto it = menu.begin(); it != menu.end(); ++it)
+//    {
+//        MenuItem *testedItem = *it;
+//        uint32_t testedRight = testedItem->getItemId();
+//        if (rightId == testedRight)
+//        {
+//            //In our context we consider Left as Previous
+//            if (processedMenuItem->getNext() == nullptr)
+//            {
+//                processedMenuItem->setNext(testedItem);
+//            }
+//            if (testedItem->getPrevious() == nullptr)
+//            {
+//                testedItem->setPrevious(processedMenuItem);
+//            }
+//        }
+//    }
+//}
 }

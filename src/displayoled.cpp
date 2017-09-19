dabf6ac42610bfb8fbfe8eb1166fd91b295eded1
Yurii Zhurakovskyi <yzhurakovskyi@luxoft.com>
2017-09-19 22:21:38 +0800

    Fake image to display 4

--- src/displayoled.cpp
+++ src/displayoled.cpp
@@ -176,6 +176,24 @@ void DisplayOled::resetCurrentActivePosition()
 
 void DisplayOled::drawBitmap(int16_t x, int16_t y, const uint8_t *bitmap, int16_t w, int16_t h, uint16_t color)
 {
+    unsigned char logo_bmp[] =
+    { 0b00000000, 0b11000000,
+      0b00000001, 0b11000000,
+      0b00000001, 0b11000000,
+      0b00000011, 0b11100000,
+      0b11110011, 0b11100000,
+      0b11111110, 0b11111000,
+      0b01111110, 0b11111111,
+      0b00110011, 0b10011111,
+      0b00011111, 0b11111100,
+      0b00001101, 0b01110000,
+      0b00011011, 0b10100000,
+      0b00111111, 0b11100000,
+      0b00111111, 0b11110000,
+      0b01111100, 0b11110000,
+      0b01110000, 0b01110000,
+      0b00000000, 0b00110000 };
+
     uint8_t icons[NUMFLAKES][3];
     srandom(666);     // whatever seed
     (void)bitmap;

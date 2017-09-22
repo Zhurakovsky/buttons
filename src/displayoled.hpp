b976c21aabbcc3c9a7d3b4aa4a1d43c28ed19293
Yurii Zhurakovskyi <yzhurakovskyi@luxoft.com>
2017-09-22 22:16:39 +0800

    Pass bitmap to OLED as vector 4

--- src/displayoled.hpp
+++ src/displayoled.hpp
@@ -49,7 +49,7 @@ public:
     void decreaseCurrentActivePosition(const std::vector<MenuItem *> &menuItems);
     void resetCurrentActivePosition();
 
-    void drawBitmap(int16_t x, int16_t y, const uint8_t *bitmap, int16_t w, int16_t h, uint16_t color);
+    void drawBitmap(int16_t x, int16_t y, const std::vector<uint8_t>&bitmap, int16_t w, int16_t h, uint16_t color);
     void drawBitmap(int16_t shift_x, int16_t shift_y);
     void clear();
     void printText(const std::string &textToPrint);
